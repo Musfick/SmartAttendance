4084e039f39b1069ceb830bd2e6a59732c1d59eb
Musfick <jamil15-9833@diu.edu.bd>
2019-07-16 02:19:29 -0700

    New package added and MainActivity has been moved.

--- app/src/main/java/com/teamcreative/smartattendance/Activity/MainActivity.java
+++ app/src/main/java/com/teamcreative/smartattendance/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.teamcreative.smartattendance;
+package com.teamcreative.smartattendance.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.teamcreative.smartattendance.R;
 
 public class MainActivity extends AppCompatActivity {
 
